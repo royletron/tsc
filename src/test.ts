caa010f367f4752284c90817c803d1931c8c462a
Darren Royle <darren.royle@diffblue.com>
2018-10-19 15:03:46 +0100

    Testing sets

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,8 @@
 export function superAmazingFunction (input: number): number {
   return input / 2;
 }
+
+export function madSetThing (numbers: number[]): boolean {
+  let numberSet = new Set(numbers);
+  return numbers.length === numberSet.size;
+}
--- test/test.ts
+++ test/test.ts
@@ -2,11 +2,19 @@ import * as Assert from 'assert';
 
 import 'mocha';
 
-import { superAmazingFunction } from '../src/test';
+import { superAmazingFunction, madSetThing } from '../src/test';
 
 describe('super thing', () => {
   it('should give me 2 from 4', () => {
     const result = superAmazingFunction(4);
     Assert.equal(result, 2);
+  });
+  it('should get a good set', () => {
+    const result = madSetThing([1, 2, 3, 4]);
+    Assert.equal(result, true);
+  });
+  it('should get a bad set', () => {
+    const result = madSetThing([1, 1, 3, 4]);
+    Assert.equal(result, false);
   });
 });
